4930cc511a6a69395e619b1a16f92b9b54f15358
Calvin Chu <calvinchu8172@gmail.com>
2015-03-27 12:16:55 +0800

    add new form to event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,8 @@ class EventsController < ApplicationController
     # @events = Event.all
     @events = Event.page(params[:page]).per(5)
 
+    @event = Event.new
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml { render :xml => @events.to_xml }
