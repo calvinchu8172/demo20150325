102acede0430a0cf603975d48a5599803490b40e
Calvin Chu <calvinchu8172@gmail.com>
2015-03-27 16:51:30 +0800

    add more bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def icon(name)
+    "<span class=\"glyphicon glyphicon-#{name}\"></span>".html_safe
+  end
+
+  def event_status_options
+    [["發佈","published"],["草稿","draft"]]
+  end
+
 end
---
+++ db/migrate/20150327045158_add_column_to_events.rb
@@ -0,0 +1,6 @@
+class AddColumnToEvents < ActiveRecord::Migration
+  def change
+    add_column :events, :published_at, :datetime
+    add_column :events, :due_date, :date
+  end
+end
